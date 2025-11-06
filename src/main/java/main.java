bd39a2a87a96fc3e735bb6876d3a711a78897b8f
tirox67 <166658856+tirox67@users.noreply.github.com>
2025-11-06 13:49:53 +0100

    feat: initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+public class main extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Messages.showMessageDialog("Hello from my plugin!", "Yay!", Messages.getInformationIcon());
+    }
+}
