ae5578297721c81c1df2dc23e840220fae3f845e
GitHub Action <action@github.com>
2025-11-06 12:24:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/timurbanczyk/cosaclassificationofsecurityapis/MyBundle.kt
+++ src/main/kotlin/com/github/timurbanczyk/cosaclassificationofsecurityapis/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.timurbanczyk.cosaclassificationofsecurityapis
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
